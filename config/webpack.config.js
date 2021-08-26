2ebdd7210fec13a71c5520862ce25c7b7b3cf50d
Sadame <azumax118sada@gmail.com>
2021-08-26 10:20:06 +0900

    コンパイル設定を追加

---
+++ config/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './src/client/index.html',
+  filename: './index.html',
+})
+module.exports = {
+  entry: './src/client/index.tsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+}
