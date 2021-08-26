5edf681488df5bff1a8cdcade470f8b8f6cfeb95
Sadame <azumax118sada@gmail.com>
2021-08-27 02:29:38 +0900

    npm run lint:fix

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -1,37 +1,37 @@
-import * as express from 'express'
-import * as admin from 'firebase-admin'
+import * as express from 'express';
+import * as admin from 'firebase-admin';
 
 /* eslint new-cap: 0 */
-export const usersRouter = express.Router()
+export const usersRouter = express.Router();
 
-const userCollection = 'users'
-const db = admin.firestore()
+const userCollection = 'users';
+const db = admin.firestore();
 
 interface User {
-  firstName: string
-  lastName: string
-  email: string
-  areaNumber: string
-  department: string
-  id: string
-  contactNumber: string
+  firstName: string;
+  lastName: string;
+  email: string;
+  areaNumber: string;
+  department: string;
+  id: string;
+  contactNumber: string;
 }
 
 usersRouter.get('/', async (req, res, next) => {
   try {
-    const userQuerySnapshot = await db.collection(userCollection).get()
-    const users: any[] = []
+    const userQuerySnapshot = await db.collection(userCollection).get();
+    const users: any[] = [];
     userQuerySnapshot.forEach((doc) => {
       users.push({
         id: doc.id,
         data: doc.data(),
-      })
-    })
-    res.status(200).json(users)
+      });
+    });
+    res.status(200).json(users);
   } catch (error) {
-    res.status(500).send(error)
+    res.status(500).send(error);
   }
-})
+});
 
 // Create new user
 usersRouter.post('/', async (req, res) => {
@@ -44,27 +44,27 @@ usersRouter.post('/', async (req, res) => {
       department: req.body['department'],
       id: req.body['id'],
       contactNumber: req.body['contactNumber'],
-    }
+    };
 
-    const newDoc = await db.collection(userCollection).add(user)
-    res.status(201).send(`Created a new user: ${newDoc.id}`)
+    const newDoc = await db.collection(userCollection).add(user);
+    res.status(201).send(`Created a new user: ${newDoc.id}`);
   } catch (error) {
-    res.status(400).send('error: create user')
+    res.status(400).send('error: create user');
   }
-})
+});
 
 // get a single contact
 usersRouter.get('/:userId', (req, res) => {
-  const userId = req.params.userId
+  const userId = req.params.userId;
   db.collection(userCollection)
     .doc(userId)
     .get()
     .then((user) => {
-      if (!user.exists) throw new Error('User not found')
-      res.status(200).json({ id: user.id, data: user.data() })
+      if (!user.exists) throw new Error('User not found');
+      res.status(200).json({ id: user.id, data: user.data() });
     })
-    .catch((error) => res.status(500).send(error))
-})
+    .catch((error) => res.status(500).send(error));
+});
 
 // Delete a user
 usersRouter.delete('/:userId', (req, res) => {
@@ -73,9 +73,9 @@ usersRouter.delete('/:userId', (req, res) => {
     .delete()
     .then(() => res.status(204).send('Document successfully deleted!'))
     .catch(function (error) {
-      res.status(500).send(error)
-    })
-})
+      res.status(500).send(error);
+    });
+});
 
 // Update user
 usersRouter.put('/:userId', async (req, res) => {
@@ -84,5 +84,5 @@ usersRouter.put('/:userId', async (req, res) => {
     .doc(req.params.userId)
     .set(req.body, { merge: true })
     .then(() => res.json({ id: req.params.userId }))
-    .catch((error) => res.status(500).send(error))
-})
+    .catch((error) => res.status(500).send(error));
+});
