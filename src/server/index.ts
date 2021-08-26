5419cf3f8cedf8f0bfbd16ff33b77bfd4bb699d8
Sadame <azumax118sada@gmail.com>
2021-08-27 00:21:02 +0900

    階層を移動

---
+++ src/server/index.ts
@@ -0,0 +1,22 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+
+// initialize firebase inorder to access its services
+admin.initializeApp(functions.config().firebase)
+
+import { usersRouter } from './routes/users'
+
+// initialize express server
+const app = express()
+const main = express()
+
+main.use('/api/v1', app)
+main.use(bodyParser.json())
+main.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/users', usersRouter)
+
+// define google cloud function name
+export const webApi = functions.https.onRequest(main)
